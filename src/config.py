ef83081261a4b6a808ae57a64717b0306a159223
Gabriel Mello <gabriel.mellosjb@gmail.com>
2026-04-16 15:25:48 -0300

    Normalização das resposta, melhorias de legibilidade, maior resistência a falhas de configuração

--- src/config.py
+++ src/config.py
@@ -26,19 +26,34 @@ SENHA_PORTAL = environ.get('SENHA_PORTAL', '').strip('"')
 IMPRESSORA   = environ.get('IMPRESSORA_PEDIDO', '').strip('"')
 
 if not UNRAR_TOOL.exists():
-    encerrar_por_erro('caminho do utilitário de .RAR não é válido.', UNRAR_TOOL)
+    encerrar_por_erro(
+        'Caminho do utilitário de .RAR não é válido.',
+        UNRAR_TOOL
+    )
 
 if not SUMATRA.exists():
-    encerrar_por_erro('caminho do utilitário SumatraPDF não é válido.', SUMATRA)
+    encerrar_por_erro(
+        'Caminho do utilitário SumatraPDF não é válido.',
+        SUMATRA
+    )
 
 if not BASE_PATH_PEDIDOS:
-    encerrar_por_erro('caminho onde os arquivos de importação serão salvos não está configurado.', 'Variável de ambiente HAVAN_PEDIDOS')
+    encerrar_por_erro(
+        'Caminho dos arquivos de importação não configurado.',
+        'Variável de ambiente HAVAN_PEDIDOS'
+    )
 
 if not CNPJ_MATRIZ or not SENHA_PORTAL:
-    encerrar_por_erro('Credenciais para o site da Havan não configuradas.', 'Variáveis de ambiente CNPJ_MATRIZ, SENHA_PORTAL')
+    encerrar_por_erro(
+        'Credenciais para o site da Havan não configuradas.',
+        'Variáveis de ambiente CNPJ_MATRIZ, SENHA_PORTAL'
+    )
 
 if not IMPRESSORA:
-    encerrar_por_erro('Impressora dos pedidos não configurada.', 'Variável de ambiente IMPRESSORA_PEDIDO')
+    encerrar_por_erro(
+        'Impressora dos pedidos não configurada.',
+        'Variável de ambiente IMPRESSORA_PEDIDO'
+    )
 
 
 USER_AGENT = (
@@ -49,23 +64,46 @@ USER_AGENT = (
 
 ORIGIN       = 'https://cliente.havan.com.br'
 BASE_URL     = f'{ORIGIN}/Fornecedor'
-CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=UTF-8'
-
+CONTENT_TYPE = (
+    'application/x-www-form-urlencoded; charset=UTF-8'
+)
 
 CAMPOS = {
     'sisplan': 'Sisplan - 002 - NUNES ENXOVAIS IND, COM, IMP E EXP LTDA',
-    'operacao': 55, 'cliente': 53, 'representante': 51, 'tabela_preco': 49,
-    'transporte': 48, 'prazo_entrega': 28, 'prazo_producao': 26,
-    'historico': 18, 'tipo_venda': 16, 'numero': 13, 'ped_cliente': 12,
-    'saida': 6, 'entrega': 7, 'fatura': 8, 'classe_gerencial': 1, 'grid': 2,
-    'nome': 0, 'combo_empresa': 0
+    'operacao': 55,
+    'cliente': 53,
+    'representante': 51,
+    'tabela_preco': 49,
+    'transporte': 48,
+    'prazo_entrega': 28,
+    'prazo_producao': 26,
+    'historico': 18,
+    'tipo_venda': 16,
+    'numero': 13,
+    'ped_cliente': 12,
+    'saida': 6,
+    'entrega': 7,
+    'fatura': 8,
+    'classe_gerencial': 1,
+    'grid': 2,
+    'nome': 0,
+    'combo_empresa': 0
 }
 
 PRODUTOS_GOVERNADOR = {
-    'DIVERTIDA', 'FORMATO', 'ASSENTO CADEIRA', 'GUARDANAPO',
-    'AVENTAL', 'TROCADOR', 'ALMOFADA DECOR 38X38 HAVAN BABY',
-    'AMAMENTAÇÃO', 'DESCANSO', 'MINI', 'GUIRLANDA', 'ABRAÇO',
-    'PESO DE PORTA','ALMOFADA QUADRADA 38X38 TURMA DO ABRACO'
+    'ALMOFADA DECOR 38X38 HAVAN BABY',
+    'ALMOFADA QUADRADA 38X38 TURMA DO ABRACO',
+    'ALMOFADA TURMA DO ABRACO',
+    'AMAMENTACAO',
+    'ASSENTO CADEIRA',
+    'AVENTAL',
+    'DESCANSO',
+    'DIVERTIDA',
+    'GUARDANAPO',
+    'GUIRLANDA',
+    'MINI ALMOFADA',
+    'PESO DE PORTA',
+    'TROCADOR',
 }
 
 COORD_ABA_PEDIDO = (87, 10)
