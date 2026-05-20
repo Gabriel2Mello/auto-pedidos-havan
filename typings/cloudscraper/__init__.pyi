4c53efb4d0defe8b9f2b56e32ee145f110f5ebea
Gabriel Mello <gabriel.mellosjb@gmail.com>
2026-05-20 14:36:36 -0300

    Adicionado arquivo Typings para lidar com os tipos do cloudscraper

--- typings/cloudscraper/__init__.pyi
+++ typings/cloudscraper/__init__.pyi
@@ -1,3 +1,24 @@
-from typing import Any
+from typing import Any, Type, Optional
+from types import TracebackType
 
-def create_scraper(*args: Any, **kwargs: Any) -> Any: ...
+class ResponseMock:
+    text: str
+    content: bytes
+    status_code: int
+
+    def raise_for_status(self) -> None: ...
+
+class ScraperMock:
+    headers: dict[str, str]
+
+    def get(self, url: str, **kwargs: Any) -> ResponseMock: ...
+    def post(self, url: str, **kwargs: Any) -> ResponseMock: ...
+    def __enter__(self) -> 'ScraperMock': ...
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType]
+    ) -> Optional[bool]: ...
+
+def create_scraper(*args: Any, **kwargs: Any) -> ScraperMock: ...
